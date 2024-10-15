6fc6f9342b74567f00df8e097cc2737687c6b7c9
CrescentLua <johnanoya.com@gmail.com>
2024-10-15 09:41:02 -0400

    google translate implementation

--- misc_cog.py
+++ misc_cog.py
@@ -3,6 +3,9 @@ import random
 
 from discord import app_commands
 from discord.ext import commands
+from googletrans import Translator
+
+translator = Translator()
 
 async def setup(bot): 
     await bot.addcog(misc_cog(bot))
@@ -10,6 +13,11 @@ async def setup(bot):
 class misc_cog(commands.Cog):
     def __init__(self, bot): 
         self.bot = bot
+        self.ctx_menu = app_commands.ContextMenu(
+          name='Translate Text',
+          callback=self.translate, 
+        )
+        self.bot.tree.add_command(self.ctx_menu)
     
     @commands.command() 
     async def roll(self, ctx, num = 99):
@@ -38,3 +46,11 @@ class misc_cog(commands.Cog):
       osmows_embed.set_image(url = misc_cog.osmows_images[rand_img])
 
       await interaction.response.send_message(embed = osmows_embed)
+
+    @app_commands.allowed_installs(guilds = True, users = True)
+    @app_commands.allowed_contexts(guilds = True, dms = True, private_channels = True)
+    async def translate(self, interaction: discord.Interaction, message: discord.Message) -> None:
+       result = translator.translate(message.content, dest='en')
+       await interaction.response.send_message("Translation: " + result.text, ephemeral=True) 
+       #await interaction.user.send(result.text)
+    
